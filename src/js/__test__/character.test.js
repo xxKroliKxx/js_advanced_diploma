1cac4d6be1ff16694a19598af1a29c7cf5deeaa0
rka <russkikh_k@ekb.sima-land.ru>
2021-12-19 20:09:20 +0500

    +

--- src/js/__test__/character.test.js
+++
@@ -1,7 +0,0 @@
-import Character from '../Character';
-
-test('character creation', () => {
-  expect(() => {
-    new Character(100, 'bowman', 100, 100, false); // eslint-disable-line  no-new
-  }).toThrow(new Error('Creating a class is prohibited'));
-});
